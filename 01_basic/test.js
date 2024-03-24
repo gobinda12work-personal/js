f2e4c27656df435546642da9efcc91a257f20543
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-03-24 14:59:16 +0000

    practice javaScript

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,69 @@
-console.log("Gobinda")
+let score = "34abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber= Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+let isLogedIn= "Gobin"
+let booleanisLogedIn= Boolean(isLogedIn)
+
+// console.log(booleanisLogedIn)
+
+// 1=> true
+// 0=> false
+// ""=> false
+// "Gobin"=> true
+
+let num1= 20
+let strnum1= String(num1)
+// console.log(strnum1)
+// console.log(typeof strnum1)
+
+// *******************  operators *********************
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/4)
+// console.log(2%4)
+
+const str1= "Gobin"
+const str2= "Barman"
+
+// const str3 = str1 + str2
+// console.log(str3)
+
+// console.log(`My name is ${str1} ${str2}`)
+
+
+// Primitive Data Type
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non-Primitive (Reference)
+// Array, Objects, Functions
+
+// 10, 10.3 => both are considered as number there is no any category of float
+
+const isLogedIn1= false
+const temp2= null
+let userEmail;
+let userEmail1= undefined
+
+const id= Symbol("abc")
+const anotherId= Symbol("abc")
+
+// console.log(id)
+// console.log(anotherId)
+// console.log(id===anotherId)
+
+const bigNumber= 78924356748974882n
+console.log(typeof bigNumber);
+console.log(bigNumber);
+
+
+
+
