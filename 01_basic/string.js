acd92ff0f2193f571f79636b3fdb6018d9a4c322
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-03-25 13:39:20 +0000

    topic string

---
+++ 01_basic/string.js
@@ -0,0 +1,28 @@
+const str= "Gobinda Barman is a Computer Science Student"
+
+// console.log(str.endsWith('a'))
+
+//console.log(str.fontcolor('red'))
+
+// console.log(str.includes('id')) => false
+// console.log(str.includes('in')) => true
+
+// console.log(str.indexOf('nd'))  => 4
+// console.log(str.indexOf('n'))  => 4
+
+// console.log(str.lastIndexOf('Computer')) => 20
+// console.log(str.indexOf('Computer')) => 20
+
+const str1= "Gobin"
+const str2= "     GOBIN     BARMAN"
+
+// console.log(str1.localeCompare(str2))
+
+// console.log(str2.trimStart())  => GOBIN     BARMAN
+// console.log(str2.trimLeft())  =>GOBIN     BARMAN
+// console.log(str2.trimRight())  =>     GOBIN     BARMAN
+// console.log(str2.trimEnd())  =>      GOBIN     BARMAN
+
+console.log(str1.toUpperCase())
+console.log(str1.toLowerCase())
+
