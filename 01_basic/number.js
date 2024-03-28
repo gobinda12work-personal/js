b793f50762958c03963b995615409f37f9689367
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-03-28 14:40:05 +0000

    number and math

---
+++ 01_basic/number.js
@@ -0,0 +1,33 @@
+const num1= new Number(200)
+// console.log(num1)
+// console.log(num1.toFixed(3.1))
+
+// console.log(num1.toString().length)
+// console.log(num1.toString())
+// console.log(typeof num1.toString())
+
+const num2= 23.8967
+
+// console.log(num2.toPrecision(3))
+
+const num3= 10000000
+// console.log(num3.toLocaleString('en-IN'))
+
+// console.log(Math.abs(-10))
+// console.log(Math.round(4.4))
+// console.log(Math.round(4.5))
+// console.log(Math.round(4.9))
+// console.log(Math.floor(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.1))
+// console.log(Math.ceil(4.8))
+
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(5,2))
+// console.log(Math.min(4,23,45,6,9,49))
+// console.log(Math.max(4,23,45,6,9,49))
+
+const min =10
+const max =20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
