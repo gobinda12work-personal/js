7a2527c48120ba50449a3fe81c09d58889dd874a
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-04-04 03:32:23 +0000

    functions

---
+++ 02_basic/function.js
@@ -0,0 +1,30 @@
+// function myName(){
+//     console.log('Gobinda Barman')
+//     console.log('Akash Barman')
+//     console.log('Rakesh Barman')
+// }
+
+// myName()
+
+// function addTwoNumber(num1,num2){
+//     console.log(num1 + num2);
+// }
+
+// addTwoNumber(4,8)
+// addTwoNumber(4,'8')
+
+// function addTowNumber(num1,num2){
+//     // let result= num1 + num2
+//     // return result
+//     return num1 + num2
+// }
+
+// console.log(addTowNumber(4,5));
+
+/****************Rest Operator*****************/
+
+function addNumbers(...num){
+    return num
+}
+
+console.log(addNumbers(5,9,90,100));
