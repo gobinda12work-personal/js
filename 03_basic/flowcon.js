8b2b7534bcc796db46193d185fafae488af318ac
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-04-07 01:36:07 +0000

    flow control

---
+++ 03_basic/flowcon.js
@@ -0,0 +1,35 @@
+// if (false) {
+//     console.log("Your statement is true")
+// }else{
+//     console.log("checkout the mistake")
+// }
+
+// const num = 200
+// if(num<=300){
+//     console.log("Number is less than 300")
+//     if(num>=100){
+//         console.log("Number is greater than 100")
+//     }
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <=10; j++) {
+//         const element = j
+//         console.log(element,i)
+        
+//     }
+    
+// }
+
+// let i=1
+
+// while (i<=10) {
+//     console.log(i)
+//     i++
+// }
+
