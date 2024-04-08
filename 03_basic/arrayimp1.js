742ee285407f8b3ab2ad093637374323df9af87f
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-04-08 13:39:29 +0000

    imp1

---
+++ 03_basic/arrayimp1.js
@@ -0,0 +1,13 @@
+// filter(), map(), reduce() method in array 
+
+// We use the filter() method to filter the array according to some certain condition and return it
+
+// We use the map() method to update like add, substract, multiply the elements of the array accordings to what has been said into the call back function and return it
+
+// We use the reduce() method to get the total number or total result of the elements of array according to the code what has been written inside the call back function and return it
+
+// forEach() method can't return the value
+
+
+
+
