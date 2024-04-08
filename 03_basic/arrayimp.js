e5df1fe0d10ee654641a890a489b7fba46cf5ed1
Gobinda Barman <164195719+gobinda12work-personal@users.noreply.github.com>
2024-04-08 11:31:55 +0000

    array important

---
+++ 03_basic/arrayimp.js
@@ -0,0 +1,21 @@
+// forEach method of array don't return any value 
+// if you write the return keyword though it will not work
+
+// In this topic we will discuss about { filter, map and reduce}
+
+
+const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const num= numbers.filter( (num) => num > 4)
+
+// console.log(num)
+
+/*const newNumbers= []
+
+numbers.forEach( (item) => {
+    if(item> 4){
+        newNumbers.push(item)   // We can't return the value using forEach Loop thus it is a way to store the numbers out of the scope
+    }
+})
+console.log(newNumbers) */
+
